5a7a161a3464d94fef7a6131bfe53797501ac9c4
Kimberly Bordon <kbordon@gmail.com>
2017-09-25 22:08:19 -0700

    Remove the additional address fields with submit listener event.

--- js/scripts.js
+++ js/scripts.js
@@ -33,15 +33,15 @@ $(document).ready(function() {
     $("#new-addresses").append('<div class="new-address">' +
                                '<div class="form-group">' +
                                  '<label for="new-street">Street</label>' +
-                                 '<input type="text" class="form-control new-street">' +
+                                 '<input type="address" class="form-control new-street">' +
                                '</div>' +
                                '<div class="form-group">' +
                                  '<label for="new-city">City</label>' +
-                                 '<input type="text" class="form-control new-city">' +
+                                 '<input type="address" class="form-control new-city">' +
                                '</div>' +
                                '<div class="form-group">' +
                                  '<label for="new-state">State</label>' +
-                                 '<input type="text" class="form-control new-state">' +
+                                 '<input type="address" class="form-control new-state">' +
                                '</div>' +
                              '</div>');
   });
@@ -60,6 +60,9 @@ $(document).ready(function() {
       var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
       newContact.addresses.push(newAddress);
     });
+
+    $(".new-address:not(:first)").remove();
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
